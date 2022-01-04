61f96bbda89b04ee3e4805b007a21d16287097fc
Victor Caruso <vic.caruso@gmail.com>
2022-01-03 17:58:05 -0800

    catchphrases working

--- build/build.js
+++ build/build.js
@@ -18,7 +18,7 @@ const headEl = document.getElementById('head');
 const middleEl = document.getElementById('middle');
 const bottomEl = document.getElementById('bottom');
 const reportEl = document.getElementById('report');
-const chatchphrasesEl = document.getElementById('chatchphrases');
+const catchphrasesEl = document.getElementById('catchphrases');
 const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
 const logoutButton = document.getElementById('logout');
@@ -27,6 +27,7 @@ const logoutButton = document.getElementById('logout');
 let headCount = 0;
 let middleCount = 0;
 let bottomCount = 0;
+let catchphrases = [];
 
 headDropdown.addEventListener('change', async() => {
     // increment the correct count in state
@@ -56,20 +57,21 @@ bottomDropdown.addEventListener('change', async() => {
 });
 
 catchphraseButton.addEventListener('click', async() => {
-    catchphraseInput.value = '';
-
     // go fetch the old catch phrases
-    
+    let character = await getCharacter();
+    catchphrases = character.catchphrases;
     // update the catchphrases array locally by pushing the new catchphrase into the old array
-
+    catchphrases.push(catchphraseInput.value);
     // update the catchphrases in supabase by passing the mutated array to the updateCatchphrases function
+    await updateCatchphrases(catchphrases);
     refreshData();
+    // clear catchphrase input field
+    catchphraseInput.value = '';
 });
 
 window.addEventListener('load', async() => {
     // on load, attempt to fetch this user's character
     let character = await getCharacter();
-    console.log(character);
     // if this user turns out not to have a character
     if (!character) {
         // create a new character with correct defaults for all properties (head, middle, bottom, catchphrases)
@@ -82,8 +84,8 @@ window.addEventListener('load', async() => {
 
         character = await createCharacter(defaultCharacter);
     } 
-    console.log(character);
     // and put the character's catchphrases in state (we'll need to hold onto them for an interesting reason);
+    catchphrases = character.catchphrases;
     // then call the refreshData function to set the DOM with the updated data
     refreshData();
 });
@@ -114,6 +116,14 @@ async function fetchAndDisplayCharacter() {
         bottomEl.style.backgroundImage = `url(../assets/${character.bottom}-pants.png)`;
     }
     // loop through catchphrases and display them to the dom (clearing out old dom if necessary)
+    while (catchphrasesEl.firstChild) {
+        catchphrasesEl.firstChild.remove();
+    }
+    for (let catchphrase of character.catchphrases) {
+        const p = document.createElement('p');
+        p.textContent = catchphrase;
+        catchphrasesEl.append(p);
+    }
 }
 
 function refreshData() {
